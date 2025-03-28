13b6aa5bbc0822231da6b1eefe04441db36a5c72
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2025-03-28 13:19:46 +0530

    Added clerk

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div> 
       Hello, this is aryan kashyap
+      <Button>
+        Click it
+      </Button>
     </div>
   );
 }
