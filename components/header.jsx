367452356927fb39ddbfc95294ede9cde7201f03
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2025-03-28 16:06:37 +0530

    Added Auth using clerk

---
+++ components/header.jsx
@@ -0,0 +1,12 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+
+ const Header = async () => {
+   
+   return (
+    <header className="flex justify-end items-center p-4 gap-4 h-16">
+    Header
+  </header>
+   );
+ };
+ 
+ export default Header;
