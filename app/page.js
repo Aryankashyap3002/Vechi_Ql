f6a1bebbc49ab2e2d9db503c2ab4286d602d9475
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2025-04-01 01:30:23 +0530

    Added car listing and filters api

--- app/page.js
+++ app/page.js
@@ -10,11 +10,14 @@ import  { HomeSearch }  from "@/components/home-search";
 import { Button } from "@/components/ui/button";
 import { CarCard } from "@/components/car-card";
 import Link from "next/link";
-import { featuredCars } from "@/lib/data";
 import { bodyTypes, carMakes, faqItems } from "@/lib/data";
+import { getFeaturedCars } from "@/actions/home";
 
 
-export default function Home() {
+export default async function Home() {
+
+  const featuredCars = await getFeaturedCars();
+
   return (
     <div className="flex flex-col pt-20"> 
      {/* Hero */}
